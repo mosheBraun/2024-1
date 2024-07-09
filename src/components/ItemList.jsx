7d064c84b8b127918e708c0da76703fade8464e0
Zalman Shlisel <yyshlisel@gmail.com>
2024-07-09 18:55:31 -0400

    spelling mistake

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -5,7 +5,7 @@ function ItemList({items, price}){
     const FilterPrice = price === null ? items : items.filter(item => item.price <= price)
 
     if (FilterPrice.length === 0){
-        return <p>No users match the filter criteria.</p>;
+        return <p>No products match the filter criteria.</p>;
 
     }
     return (
